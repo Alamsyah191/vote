2477408705c3aac6ca23ace0788e97702d940a1c
010901alam <010901alam@gmail.com>
2022-03-19 08:25:08 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Demo Application</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap-notifications.min.css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <div id="div-data"></div>
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    {{-- <script src="//js.pusher.com/3.1/pusher.min.js"></script> --}}
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+    <script src='./js/app.js'></script>
+    {{-- <script>
+        // Enable pusher logging - don't include this in production
+        Pusher.logToConsole = true;
+
+        var pusher = new Pusher('5b505288b96e3cd5f02e', {
+            cluster: 'ap1'
+        });
+
+        var channel = pusher.subscribe('status-liked');
+        channel.bind('StatusLiked', function(data) {
+            alert(JSON.stringify(data));
+        });
+    </script> --}}
+
+    <script>
+        window.Echo.channel('status-liked')
+            .listen('StatusLiked', (e) => {
+                console.log(e)
+                document.querySelector('#div-data').innerHTML = e.message
+            })
+    </script>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Events\StatusLiked;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test', function () {
+    event(new StatusLiked('Someone'));
+    return "Event has been sent!";
+});
+
+Route::get('/trigger/{data}', function ($data) {
+    echo "<p>You have sent $data</p>";
+    event(new StatusLiked($data));
+});
